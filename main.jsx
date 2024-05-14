8b04d322b0750080d8a8a650a7305379ad13e62a
Tamilarasan T <133289626+Tamilroottag@users.noreply.github.com>
2024-05-14 09:08:47 +0530

    main.jsx
    this file is main file to use render a card file

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { UserCard } from './Components/UserCard'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <UserCard />
+  </React.StrictMode>,
+)
